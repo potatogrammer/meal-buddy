9531f0b1107b2b451869723ac2c727e23376e4c6
Hieu Nguyen <hmn45@drexel.edu>
2020-07-16 11:17:59 +0700

    Updates

---
+++ precache-manifest.9839a43168d119bce3aaee789c95f689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe678e11db723b5788826d55a76801a",
+    "url": "/meal-buddy/index.html"
+  },
+  {
+    "revision": "53b7d7bfaa36af2a35cb",
+    "url": "/meal-buddy/static/css/main.66db7e6f.chunk.css"
+  },
+  {
+    "revision": "3a2adc5635555c3ec87e",
+    "url": "/meal-buddy/static/js/2.9a177f15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meal-buddy/static/js/2.9a177f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b7d7bfaa36af2a35cb",
+    "url": "/meal-buddy/static/js/main.2ea2b872.chunk.js"
+  },
+  {
+    "revision": "2e868996771d6a4e2151",
+    "url": "/meal-buddy/static/js/runtime-main.be3032d4.js"
+  }
+]);
